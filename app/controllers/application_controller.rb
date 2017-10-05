958a1e1d55de42bda74fd9bde0754c9a8f7d48d2
Claudia El Khatib <claudiacv22@gmail.com>
2017-10-05 17:38:34 -0400

    added :patch in rack cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,6 @@ class ApplicationController < ActionController::API
   def authenticate
     @current_user =
       authenticate_or_request_with_http_token(&AUTH_PROC)
-      binding.pry
   end
 
   # call from actions to get authenticated user (or nil)
