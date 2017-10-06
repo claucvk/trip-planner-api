fd224fdfbca35def7c60ab4b10e143783bfa5360
Claudia El Khatib <claudiacv22@gmail.com>
2017-10-05 20:11:06 -0400

    added delete in rack cors

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module TripApp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
       end
    end
 
